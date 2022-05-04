2a22a4066607a67f9f2c872d4ddc721fec385436
crist <raducristina18@stud.ase.ro>
2022-05-04 20:18:38 +0300

    backend setup update

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const cors = require('cors');
+const connection = require("./db.js");
+const bodyParser = require("body-parser");
+const messagesRouter = require("./router/messagesRouter")
+
 
 const app = express();
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 const port = process.env.PORT || 8080;
 
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-  });
+app.use('/messages', messagesRouter);
   
-  app.listen(port, () => {
+app.listen(port, () => {
     console.log(`Example app listening on port ${port}!`)
   });
+
+
